c4551a1f1b9780ba60b80730cf3a110fa551a2c0
aellym0 <joewatt95@u.nus.edu>
2021-03-09 01:18:01 +0800

    Fix implementation of context module.

--- lib/kernel/context.ml
+++ lib/kernel/context.ml
@@ -21,13 +21,13 @@ sig
   val get_type : int -> t -> Ast.expr
 end
 
-module context : CONTEXT =
+module Context : CONTEXT =
 struct
   type entry = {
     var_name : string;
     var_type : Ast.expr;
     binding  : Ast.expr option
-  } [@@deriving make, show]
+  }
 
   exception UnknownVar
   exception UnknownIndex
@@ -36,7 +36,9 @@ struct
   let empty = BatVect.empty
   let is_empty = BatVect.is_empty
   let length = BatVect.length
-  let add_binding = make_entry %> BactVect.prepend
+
+  let add_binding ~var_name ~var_type ?binding =
+    BatVect.prepend {var_name; var_type; binding}
 
   let get_entry index ?(exc = BatVect.Out_of_bounds) ctx =
     try
@@ -45,19 +47,20 @@ struct
       raise exc
 
   let var_name_to_index string ctx =
+    let open Stdlib in
     try
       BatVect.findi (fun {var_name; _} -> var_name = string) ctx
     with Not_found -> raise UnknownIndex
 
   let index_to_var_name index ctx =
-    let {var_name; _} = get_entry index ?exc:UnknownVar ctx in
+    let {var_name; _} = get_entry index ~exc:UnknownVar ctx in
     var_name
 
   let get_binding index ctx =
     let {binding; _} = get_entry index ctx in
     binding
 
-  let get_type =
+  let get_type index ctx =
     let {var_type; _} = get_entry index ctx in
     var_type
 end
