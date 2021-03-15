f6fcf1d7e3b82f3dfda70b561b01eb94302ce2ff
aellym0 <joewatt95@u.nus.edu>
2021-03-15 10:57:08 +0800

    Add support for single-line comments and unicode characters in lexer.ml

--- lib/parsing/lexer.ml
+++ lib/parsing/lexer.ml
@@ -1,28 +1,51 @@
+open Containers
+
 let whitesp = [%sedlex.regexp? ' ' | '\t']
 let newline = [%sedlex.regexp? ('\r' | '\n' | "\r\n")]
-let var_name = [%sedlex.regexp? (alphabetic |  '_'), Star (alphabetic | '_' | '0' .. '9')]
+let name = 
+  [%sedlex.regexp? ((alphabetic |  '_'), Star (alphabetic | '_' | '0' .. '9')) 
+                   | math]
+
+(* Change this to change the encoding. *)
+module Encoding = Sedlexing.Utf8
 
 module G = Grammar
+let reserved_keywords = [
+  (["fun"; "λ"], G.FUN);
+  (["Pi"; "Π"; "∏"; "∀"], G.PI);
+  (["Type"], G.TYPE);
+  (["let"], G.LET);
+  (["in"], G.IN);
+  (["def"], G.DEF);
+  (["axiom"], G.AXIOM);
+  (["check"], G.CHECK);
+  (["eval"], G.EVAL);
+]
+
+(* Create a hash table mapping strings to tokens out of the above associative
+list defining reserved keywords.
+*)
+let reserved_keywords' = Hashtbl.create @@ List.length reserved_keywords;;
+List.iter
+  (fun (lst, token) ->
+    List.iter (fun str -> Hashtbl.add reserved_keywords' str token) lst)
+  reserved_keywords
 
 let rec tokenize lexbuf =
   match%sedlex lexbuf with
   | eof -> G.EOF
-  | "fun" -> G.FUN
-  | "Pi" -> G.PI
-  | "Type" -> G.TYPE
-  | "let" -> G.LET
-  | "in" -> G.IN
+  (* Support single-line comments. Idea taken from
+  https://github.com/vshaxe/hxparser/blob/master/src/syntax/lexing/lexer.ml *)
+  | "//", Star (Compl ('\r' | '\n')) -> Sedlexing.new_line lexbuf; tokenize lexbuf 
   | '(' -> G.LPAREN
   | ')' -> G.RPAREN
   | ',' -> G.COMMA
   | ':' -> G.COLON
   | ":=" -> G.COLON_EQ
   | "=>" -> G.DOUBLE_ARROW
-  | "def" -> G.DEF
-  | "axiom" -> G.AXIOM
-  | "check" -> G.CHECK
-  | "eval" -> G.EVAL
-  | var_name -> G.VAR_NAME (Sedlexing.Latin1.lexeme lexbuf)
-  | Plus whitesp -> tokenize lexbuf
   | newline -> Sedlexing.new_line lexbuf; tokenize lexbuf
+  | name -> 
+    let str = Encoding.lexeme lexbuf in
+    Hashtbl.get_or reserved_keywords' str ~default:(G.VAR_NAME str)
+  | Plus whitesp -> tokenize lexbuf
   | _ -> assert false
